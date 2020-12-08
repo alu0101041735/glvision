e174fbb50b6c7488d1c32ea6615c8d7b758f7f21
Daniel <alu0101041735@ull.edu.es>
2020-12-08 13:31:47 +0000

    Refactorized all class, brightness and contrast function created

--- include/nativeprocessor.h
+++ include/nativeprocessor.h
@@ -10,28 +10,47 @@
 #include <unordered_map>
 #include <utility>
 #include <set>
-
+#include <math.h>
 
 class NativeProcessor
 {
 private:
     QImage m_image;
+    QImage m_grayimage;
     QImage m_rimage;
+
+    int m_width;
+    int m_height;
+
+    std::vector<uint16_t> m_histogram;
+    std::vector<uint16_t> m_cumulativehistogram;
+
+    std::pair<int, int> m_valuerange;
+
+    int m_brightness;
+    int m_contrast;
 
     bool toGrayScale();
     bool toGaussian();
 
+    void computeHistogram();
+    void computeCumulativeHistogram();
+    void computeValueRange();
+    void computeBrightness();
+    void computeContrast();
+
 public:
-    NativeProcessor();
-    QImage processImage(QImage image, int transformation);
-    QImage processImage(int transformation);
-    void setImage(QImage image);
-    int getWidth(QImage image);
-    int getHeight(QImage image);
-    std::vector<uint16_t> getHistogram(QImage image);
-    std::vector<uint16_t> getCumulativeHistogram(QImage image);
-    std::pair<int, int> valueRange(QImage image);
-    std::pair<QColor, int> grayLevel(QImage image, int x, int y);
+    NativeProcessor(QImage image);
+    QImage processImage( int transformation);
+    void setImage();
+    int getWidth();
+    int getHeight();
+    std::vector<uint16_t> getHistogram();
+    std::vector<uint16_t> getCumulativeHistogram();
+    std::pair<int, int> valueRange();
+    std::pair<QColor, int> grayLevel(int x, int y);
+    int brightness();
+    int contrast();
 
 };
 
--- src/nativeprocessor.cpp
+++ src/nativeprocessor.cpp
@@ -10,6 +10,8 @@ bool NativeProcessor::toGrayScale()
     int blue;
     int result_color;
 
+    new(&m_grayimage) QImage(width, height, QImage::Format_RGBA64);
+
    for (int i = 0; i < height; i++){
        for (int j = 0; j < width; j++) {
            red = m_image.pixelColor(j, i).red();
@@ -22,25 +24,127 @@ bool NativeProcessor::toGrayScale()
            color.setGreen(result_color);
            color.setBlue(result_color);
 
-           m_rimage.setPixelColor(j, i, color);
+           m_grayimage.setPixelColor(j, i, color);
        }
    }
 
    return true;
 }
 
-NativeProcessor::NativeProcessor()
+void NativeProcessor::computeHistogram()
 {
+    new(&m_histogram) std::vector<uint16_t>(255,0);
+
+    if (m_grayimage.isNull())
+    {
+        exit(-1);
+    }
+
+    int gray_level;
+
+    for (int y = 0; y < m_height; y++) {
+        for (int x = 0; x < m_width; x++) {
+            gray_level = m_rimage.pixelColor(x, y).red();
+
+            m_histogram[gray_level] += 1;
+        }
+    }
+
 
 }
 
-QImage NativeProcessor::processImage(QImage image, int transformation)
+void NativeProcessor::computeCumulativeHistogram()
 {
-    int width = image.width();
-    int height = image.height();
+    if (m_histogram.size() == 0) {
+        computeHistogram();
+    }
+    new(&m_cumulativehistogram) std::vector<uint16_t>(255,0);
 
-    m_image = image;
-    new(&m_rimage) QImage(width, height, QImage::Format_RGBA64);
+    for (unsigned long i = 0; i < m_histogram.size(); i++) {
+        m_cumulativehistogram[i] += m_histogram[i];
+    }
+
+}
+
+void NativeProcessor::computeValueRange()
+{
+    std::pair<int, int> range;
+
+    range.first = m_grayimage.pixelColor(0,0).red();
+    range.second = m_grayimage.pixelColor(0,0).red();
+
+    for (int y = 0; y < m_height; y++) {
+        for (int x = 0; x < m_width; x++) {
+            if (m_grayimage.pixelColor(x, y).red() < range.first)
+               range.first = m_grayimage.pixelColor(x,y).red();
+            if (m_grayimage.pixelColor(x, y).red() > range.second)
+               range.second = m_grayimage.pixelColor(x,y).red();
+        }
+    }
+    m_valuerange = range;
+
+}
+
+void NativeProcessor::computeBrightness()
+{
+    if (m_histogram.size() == 0)
+        computeHistogram();
+
+    int sum = 0;
+    for (unsigned long i = 0; i < m_histogram.size(); i++) {
+
+        for (int j = 0; j < m_histogram[i]; j++) {
+            sum += i;
+        }
+    }
+
+    m_brightness = sum / (m_width*m_height);
+
+}
+
+void NativeProcessor::computeContrast()
+{
+    if (m_histogram.size() == 0)
+        computeHistogram();
+
+    if (!m_brightness) {
+        computeBrightness();
+    }
+
+    int sum = 0;
+
+    for (unsigned long i = 0; i < m_histogram.size(); i++) {
+
+        for (int j = 0; j < m_histogram[i]; j++) {
+            int aux = i - m_brightness;
+            sum += (aux*aux);
+        }
+    }
+
+    m_contrast = sqrt(sum / (m_width*m_height));
+
+}
+
+NativeProcessor::NativeProcessor(QImage image): m_image(image)
+{
+
+    m_width = image.width();
+    m_height = image.height();
+
+    bool result = toGrayScale();
+    computeHistogram();
+    computeCumulativeHistogram();
+    computeValueRange();
+    computeGrayLevel();
+    computeBrightness();
+    computeContrast();
+
+
+}
+
+QImage NativeProcessor::processImage(int transformation)
+{
+
 
     switch(transformation) {
 
@@ -65,83 +169,52 @@ QImage NativeProcessor::processImage(QImage image, int transformation)
     return m_rimage;
 }
 
-int NativeProcessor::getWidth(QImage image)
+void NativeProcessor::setImage()
 {
-    return image.width();
-}
-
-int NativeProcessor::getHeight(QImage image)
-{
-    return image.height();
-}
-
-std::vector<uint16_t> NativeProcessor::getHistogram(QImage image)
-{
-    int width = image.width();
-    int height = image.height();
-    std::vector<uint16_t> histogram(255, 0);
-
-    QImage rimage = processImage(image, 2);
-    int gray_level;
-
-    for (int y = 0; y < height; y++) {
-        for (int x = 0; x < width; x++) {
-            gray_level = rimage.pixelColor(x, y).red();
-
-            histogram[gray_level] += 1;
-        }
-    }
-
-    return histogram;
-}
-
-std::vector<uint16_t> NativeProcessor::getCumulativeHistogram(QImage image)
-{
-    int width = image.width();
-    int height = image.height();
-    std::vector<uint16_t> histogram= getHistogram(image);
-
-
-    std::vector<uint16_t> cumulative_histogram(255, 0);
-
-    for (unsigned long i = 0; i < histogram.size(); i++) {
-        cumulative_histogram[i] += histogram[i];
-    }
-
-    return cumulative_histogram;
 
 }
 
-std::pair<int, int> NativeProcessor::valueRange(QImage image)
+int NativeProcessor::getWidth()
 {
-    QImage rimage = processImage(image, 2);
-    std::pair<int, int> range;
-
-    range.first = rimage.pixelColor(0,0).red();
-    range.second = rimage.pixelColor(0,0).red();
-
-    int height = image.height();
-    int width = image.width();
-
-    for (int y = 0; y < height; y++) {
-        for (int x = 0; x < width; x++) {
-            if (rimage.pixelColor(x, y).red() < range.first)
-               range.first = rimage.pixelColor(x,y).red();
-            if (rimage.pixelColor(x, y).red() > range.second)
-               range.second = rimage.pixelColor(x,y).red();
-        }
-    }
-    return range;
+    return m_width;
 }
 
-std::pair<QColor, int> NativeProcessor::grayLevel(QImage image, int x, int y)
+int NativeProcessor::getHeight()
 {
-    QImage rimage = processImage(image, 2);
+    return m_height;
+}
 
+std::vector<uint16_t> NativeProcessor::getHistogram()
+{
+    return m_histogram;
+}
+
+std::vector<uint16_t> NativeProcessor::getCumulativeHistogram()
+{
+    return m_cumulativehistogram;
+}
+
+std::pair<int, int> NativeProcessor::valueRange()
+{
+    return m_valuerange;
+}
+
+std::pair<QColor, int> NativeProcessor::grayLevel(int x, int y)
+{
     std::pair<QColor, int> graylevel;
 
-    graylevel.first = image.pixelColor(x, y);
-    graylevel.second =  rimage.pixelColor(x, y).red();
+    graylevel.first = m_image.pixelColor(x, y);
+    graylevel.second =  m_grayimage.pixelColor(x, y).red();
 
     return graylevel;
+}
+
+int NativeProcessor::brightness()
+{
+    return m_brightness;
+}
+
+int NativeProcessor::contrast()
+{
+    return m_contrast;
 }
