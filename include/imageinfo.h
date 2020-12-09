fd64584cbe5f99223c783e4e103e980264955687
0x444d4d <alu0101053318@ull.edu.es>
2020-12-09 19:10:43 +0000

    Show information about the image

--- include/imageinfo.h
+++ include/imageinfo.h
@@ -1,11 +1,33 @@
 #ifndef IMAGEINFO_H
 #define IMAGEINFO_H
 
+#include <QString>
+#include <QPair>
+#include <QImage>
+
+typedef QPair<unsigned, unsigned> pair;
 
 class imageInfo
 {
+private:
+    QImage::Format fileType;
+    pair dimensions;
+    pair range;
+    unsigned brightness;
+    unsigned contrast;
+    unsigned entropy;
+
+
 public:
     imageInfo();
+    imageInfo(QImage::Format type, pair dim, pair range, unsigned br, unsigned cont, unsigned ent);
+    inline QImage::Format getFormat() {return fileType;}
+    inline pair getDimensions() { return dimensions;}
+    inline pair getRange() { return range;}
+    inline unsigned getBrightness() { return brightness;}
+    inline unsigned getContrast() { return contrast;}
+    inline unsigned getEntropy() { return entropy;}
+
 };
 
 #endif // IMAGEINFO_H
--- src/imageinfo.cpp
+++ src/imageinfo.cpp
@@ -4,3 +4,14 @@ imageInfo::imageInfo()
 {
 
 }
+
+imageInfo::imageInfo(QImage::Format type, pair dim, pair range, unsigned br, unsigned cont, unsigned ent) :
+    fileType(type),
+    dimensions(dim),
+    range(range),
+    brightness(br),
+    contrast(cont),
+    entropy(ent)
+{
+
+}
