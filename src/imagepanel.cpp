992379db14e78110124d3041ac569ee172d3bbf7
Daniel Paz <danielpaz4c@gmail.com>
2020-12-10 20:45:37 +0000

    Fixed call to NativeProcessing

--- src/imagepanel.cpp
+++ src/imagepanel.cpp
@@ -27,7 +27,7 @@ void imagePanel::updateInfo(imageInfo &info)
 
 void imagePanel::setHistrogram(QImage& image)
 {
-    std::vector<uint32_t> histogram = NativeProcessor(image, true).getHistogram();
+    std::vector<uint32_t> histogram = NativeProcessor(image).getHistogram();
     QBarSet* barSet = new QBarSet("histogram");
     for (auto value : histogram) {
         *barSet << value;
