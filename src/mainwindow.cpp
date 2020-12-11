cbc81553e935ed8938ed7bebe150aa17b012d656
Daniel <alu0101041735@ull.edu.es>
2020-12-11 09:38:16 +0000

    Merged

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -105,6 +105,12 @@ void MainWindow::on_actionOpen_File_triggered()
         qDebug() << "test";
     }
 
+    NativeProcessor np(images[0]);
+    QImage test2("../glvision/images/kuvshi.jpg");
+    NativeProcessor np2(test2);
+    std::vector<double> temp_histogram = np2.getNormalizedCumulativeHistogram();
+
+    np.specifyHistogram(temp_histogram).save("../glvision/images/specification_test.png");
 }
 
 void MainWindow::receieveImage(QImage &image, QString& format)
